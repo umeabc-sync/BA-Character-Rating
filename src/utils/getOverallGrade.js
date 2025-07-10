9703ae442cdecd4f443388e231d6040c6e7717e4
Yuuzi261 <lokasu0531@gmail.com>
2025-07-11 03:09:58 +0800

    style: format code

--- src/utils/getOverallGrade.js
+++ src/utils/getOverallGrade.js
@@ -1,11 +1,11 @@
 export const getOverallGrade = (score) => {
-  if (score > 100) return 'SS';
-  if (score >= 90) return 'S';
-  if (score >= 80) return 'A';
-  if (score >= 70) return 'B';
-  if (score >= 60) return 'C';
-  if (score >= 50) return 'D';
-  if (score >= 40) return 'E';
-  if (score >= 20) return 'F';
-  return 'N/A';
-};
+  if (score > 100) return 'SS'
+  if (score >= 90) return 'S'
+  if (score >= 80) return 'A'
+  if (score >= 70) return 'B'
+  if (score >= 60) return 'C'
+  if (score >= 50) return 'D'
+  if (score >= 40) return 'E'
+  if (score >= 20) return 'F'
+  return 'N/A'
+}
