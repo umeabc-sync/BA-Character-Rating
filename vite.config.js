7fa06743aaf2893989e59b8bac2023c32d95b86b
Yuuzi261 <lokasu0531@gmail.com>
2025-07-12 02:23:05 +0800

    build: add skipWaiting and clientsClaim to PWA config (exp)

--- vite.config.js
+++ vite.config.js
@@ -25,6 +25,8 @@ export default defineConfig(({ mode }) => ({
     VitePWA({ 
       registerType: 'autoUpdate',
       workbox: {
+        skipWaiting: true,
+        clientsClaim: true,
         navigateFallback: '/index.html',
         navigateFallbackAllowlist: [/.*/],
       },
