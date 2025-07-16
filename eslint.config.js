ae7e4c306c262bf80007b97a580079d3c1860461
Yuuzi261 <lokasu0531@gmail.com>
2025-07-17 03:13:32 +0800

    refactor: improve eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,40 +1,64 @@
+import globals from 'globals';
 import eslintPluginVue from 'eslint-plugin-vue';
 import js from '@eslint/js';
 import prettierConfig from 'eslint-config-prettier';
+import vueParser from 'vue-eslint-parser';
 
 export default [
   {
     ignores: ["dev-dist/*", "dist/*", "node_modules/*", "minify-json.js", ".git/*", ".vscode/*"],
   },
-  js.configs.recommended,
-  ...eslintPluginVue.configs['flat/recommended'],
-  prettierConfig,
   {
+    files: ['**/*.vue'],
     languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
+      parser: vueParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
       globals: {
-        'browser': true,
-        'node': true,
-        'es2021': true,
+        ...globals.browser,
+        ...globals.es2021,
         'console': 'readonly',
-        'window': 'readonly',
-        'document': 'readonly',
-        'fetch': 'readonly',
-        'URL': 'readonly',
-        'URLSearchParams': 'readonly',
-        'setTimeout': 'readonly',
-        'clearTimeout': 'readonly',
-        'requestAnimationFrame': 'readonly',
-        'AbortController': 'readonly',
-        'alert': 'readonly'
       }
     },
+    plugins: {
+      vue: eslintPluginVue,
+    },
     rules: {
+      ...js.configs.recommended.rules,
+      ...eslintPluginVue.configs['flat/recommended'].rules,
+      ...prettierConfig.rules,
       'vue/multi-word-component-names': 'off',
       'vue/no-unused-vars': 'warn',
       'no-unused-vars': 'warn',
       'no-undef': 'warn'
+    }
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+        'console': 'readonly',
+      }
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...prettierConfig.rules,
+      'no-unused-vars': 'warn',
+      'no-undef': 'warn'
+    }
+  },
+  {
+    files: ['scripts/**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      }
     }
   }
 ];
